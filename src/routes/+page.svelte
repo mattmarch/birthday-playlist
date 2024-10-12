aa975eee1d826c2488718776fda4be6c38ac6878
Matt March <matthewmarch29@gmail.com>
2024-10-12 10:37:56 +0100

    Add simple input type=date

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	const currentDate = new Date();
+	let selectedDateString: string | undefined = undefined;
+	$: selectedDate = selectedDateString ? new Date(selectedDateString) : undefined;
+	$: isBirthday =
+		selectedDate?.getDate() === currentDate.getDate() &&
+		selectedDate?.getMonth() === currentDate.getMonth();
+
+	const onClick = () => {
+		console.log(selectedDateString && new Date(selectedDateString));
+	};
+</script>
+
+<h1>Birthday Playlist Generator</h1>
+<p>Generate a Spotify playlist of UK number 1 singles on all your past birthdays.</p>
+<input type="date" bind:value={selectedDateString} />
+{#if isBirthday}
+	<p>Happy Birthday!</p>
+{/if}
+<div>
+	<button on:click={onClick}>Generate Playlist</button>
+</div>
