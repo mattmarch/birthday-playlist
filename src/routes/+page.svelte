16f7bb07c898e0fc57b452af0f42104fcd9f45c5
Matt March <matthewmarch29@gmail.com>
2024-11-02 13:17:11 +0000

    Duplicate button styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -116,10 +116,19 @@
 			{/if}
 			{#if populatedFromSpotify}
 				<p>Spotify data synced</p>
-				<button on:click={createPlaylist} disabled={creatingPlaylist}>Create playlist</button>
+				<button
+					on:click={createPlaylist}
+					disabled={creatingPlaylist}
+					class="text-white bg-gradient-to-r from-rose-500 via-rose-600 to-rose-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-rose-300 dark:focus:ring-rose-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+					>Create playlist</button
+				>
 			{:else}
 				<p>Spotify data not synced</p>
-				<button on:click={populateBirthdayNumberOnes}>Sync Spotify data</button>
+				<button
+					on:click={populateBirthdayNumberOnes}
+					class="text-white bg-gradient-to-r from-rose-500 via-rose-600 to-rose-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-rose-300 dark:focus:ring-rose-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+					>Sync Spotify data</button
+				>
 			{/if}
 		</div>
 	{/if}
